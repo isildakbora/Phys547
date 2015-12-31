3fd89c85eb3db03798c5ede0af981275075c8c90
isildak <bora.isildak@cern.ch>
2015-12-31 16:19:37 +0200

    initiation

---
+++ scripts/run_analysis.py
@@ -0,0 +1,15 @@
+import ROOT
+from ROOT import *
+
+file_names = ["qcd.root", "dy.root", "zz.root", "wz.root", "ww.root", "wjets.root", "ttbar.root", "single_top.root", "data.root", ]
+
+gSystem.Load("MyAnalysis.C");
+
+for inf_file in file_names:
+	print inf_file
+	f = TFile.Open(inf_file)
+	
+	cmd = 'MyAnalysis m(events, \"'+ str(inf_file) +'\")'
+	gROOT.ProcessLine(cmd)
+	print 'Processing', inf_file
+	gROOT.ProcessLine("m.Loop()")
